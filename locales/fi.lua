3b96567659b34e69e95f4d3006bf1e89596366cf
Blooper <122776166+Blo0per@users.noreply.github.com>
2023-03-26 21:19:34 +0100

    Create fi.lua
    ### _fi.lua is missing._

---
+++ locales/fi.lua
@@ -0,0 +1,17 @@
+Locales ['fi'] = {
+  ["license_shop_title"] = "Lisenssikauppa",
+  ["weapon_shop_menu_title"] = "Asekauppa",
+  ["weapon_shop_menu_description"] = "Osta aseita",
+  ["menu_cancel"] = "Peruuta",
+  ['buy_license'] = 'Osta lisenssi?',
+  ['yes'] = '%s',
+  ['no'] = 'Ei',
+  ['weapon_bought'] = 'Osta hintaan %s EUR',
+  ['not_enough_black'] = 'Ei tarpeeksi likaista rahaa',
+  ['not_enough'] = 'Ei tarpeeksi rahaa',
+  ['already_owned'] = 'Sinulla on jo tämä ase!',
+  ['shop_menu_title'] = 'Kauppa',
+  ['shop_menu_prompt'] = 'Paina [E] avataksesi kauppa valikko.',
+  ['shop_menu_item'] = '%s EUR',
+  ['map_blip'] = 'Asekauppa',
+}
