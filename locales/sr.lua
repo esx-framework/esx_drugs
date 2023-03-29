89f50346af142462daa77fd99a5432ffbad6e48a
Nemanja <71012671+NeMyOriginal@users.noreply.github.com>
2023-03-29 17:08:49 +0200

    Create sr.lua

---
+++ locales/sr.lua
@@ -0,0 +1,32 @@
+
+Locales ['sr'] = {
+  -- weed
+  ['weed_pickupprompt'] = 'Pritisni [E] da oberes kanabis.',
+  ['weed_quitprocess'] = 'Pritisni [E] da prekines preradu kanabisa.',
+  ['weed_inventoryfull'] = 'Nemas vise mesta u inventaru!',
+  ['weed_processprompt'] = 'Pritisni [E] da zapocnes preradu kanabisa.',
+  ['weed_processingstarted'] = 'Prerada kanabisa u marihuanu...',
+  ['weed_processingfull'] = 'Prerada je ~r~otkazana jer je inventar pun!',
+  ['weed_processingenough'] = 'Morate imati 3x Kanabisa da bi mogli da preradjujete.',
+  ['weed_processed'] = 'Preradili ste 3x Kanabis u 1x Marihuanu',
+  ['weed_processingtoofar'] = 'Prerada je ~r~otkazana jer ste napustili mesto prerade.',
+
+  -- drug dealer
+  ['dealer_prompt'] = 'Pritisni [E] da razgovaras sa ~r~Dilerom.',
+  ['dealer_title'] = 'Diler',
+  ['dealer_item'] = '$%s',
+  ['dealer_notenough'] = 'Nemas dovoljno za prodaju!',
+  ['dealer_sold'] = 'Prodao si %sx %s za $%s',
+
+  -- license
+  ['license_title'] = 'Moras posedovati dozvolu za preradjivanje, mozes je kupiti ovde.',
+  ['license_no'] = 'Ne',
+  ['license_bought'] = 'Kupio si %s za ~r~$%s',
+  ['license_bought_fail'] = 'Nemas dovoljno novca za %s!',
+  ['license_weed'] = 'Dozvola za Preradu Trave',
+
+  -- blips
+  ['blip_weedfield'] = 'Polje Trave',
+  ['blip_weedprocessing'] = 'Prerada Trave',
+  ['blip_drugdealer'] = 'Diler',
+}
