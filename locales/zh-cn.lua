950035aad3936f34352a0cb149a9f921d0bfda46
DyroS3 <64310374+DyroS3@users.noreply.github.com>
2023-03-29 12:23:34 +0800

    Added Simplified Chinese translation

---
+++ locales/zh-cn.lua
@@ -0,0 +1,31 @@
+Locales ['zh-cn'] = {
+  -- weed
+  ['weed_pickupprompt'] = '键下 [E] 收获大麻原叶.',
+  ['weed_quitprocess'] = '键下 [E] 暂时取消加工.',
+  ['weed_inventoryfull'] = '很抱歉! 您当前暂无足够的背包空间放置大麻.',
+  ['weed_processprompt'] = '键下 [E] 开始加工大麻原叶.',
+  ['weed_processingstarted'] = '正在加工大麻原叶...',
+  ['weed_processingfull'] = '由于背包剩余空间已满! ~r~加工已被取消!',
+  ['weed_processingenough'] = '很抱歉! 您必须拥有至少三个大麻原叶才可进行加工.',
+  ['weed_processed'] = '完成加工一个成品大麻(损耗：三个大麻原叶)'
+  ['weed_processingtoofar'] = '由于您离开了当前加工区域,此次加工过程已被自动取消.',
+
+  -- drug dealer
+  ['dealer_prompt'] = '键下 [E] 访问经销商.',
+  ['dealer_title'] = '毒品经销商',
+  ['dealer_item'] = '$%s',
+  ['dealer_notenough'] = '很抱歉!当前暂无持有足够的物品可供售卖',
+  ['dealer_sold'] = '成功售出%s个%s, 获得赃款:$%s',
+
+  -- license
+  ['license_title'] = '你需要拥有一个加工许可证才可进行加工，您可快捷支付购买.',
+  ['license_no'] = '取消购买',
+  ['license_bought'] = '已购买%s, 支付~r~$%s',
+  ['license_bought_fail'] = '您无法承担起一笔%s的支出!',
+  ['license_weed'] = '毒品加工证书',
+
+  -- blips
+  ['blip_weedfield'] = '大麻农场',
+  ['blip_weedprocessing'] = '大麻加工厂',
+  ['blip_drugdealer'] = '大麻经销商',
+}
