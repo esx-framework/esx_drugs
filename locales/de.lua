1b768b85d8a42145fe39fc043c5f0fbd140cee10
Lenny822 <84620158+Lenny822@users.noreply.github.com>
2023-04-15 12:15:42 +0200

    Create de.lua

---
+++ locales/de.lua
@@ -0,0 +1,31 @@
+Locales ['de'] = {
+  -- weed
+  ['weed_pickupprompt'] = 'Drücke [E] um Cannabis zu ernten.',
+  ['weed_quitprocess'] = 'Drücke [E] um den Cannabis fortschritt zu verwerfen.',
+  ['weed_inventoryfull'] = 'Du hast nicht genügend Platz im inventar für Cannabis.',
+  ['weed_processprompt'] = 'Drücke [E] um Cannabis zu produzieren.',
+  ['weed_processingstarted'] = 'Cannabis wird in Marijuana umgewandelt...',
+  ['weed_processingfull'] = 'Produzieren wurde ~r~abgebrochen da dein Inventar voll ist!',
+  ['weed_processingenough'] = 'Du musst 3x Cannabis besitzen um zu Produzieren.',
+  ['weed_processed'] = 'Du produzierst 3x Cannabis zu 1x Marijuana',
+  ['weed_processingtoofar'] = 'Der prozess wurde ~r~abgebrochen da du den Bereich verlassen hast!',
+
+  -- drug dealer
+  ['dealer_prompt'] = 'Drücke [E] um mit dem ~r~Drogen Dealer~r~ zu reden!',
+  ['dealer_title'] = 'Drogen Dealer',
+  ['dealer_item'] = '%s€',
+  ['dealer_notenough'] = 'Du hast davon nicht Genug um es zu Verkaufen!',
+  ['dealer_sold'] = 'Du verkaufst %sx %s für %s€',
+
+  -- license
+  ['license_title'] = 'Du brauchst eine Lizenz zum Produzieren! Eine Lizenz kannst du hier kaufen.',
+  ['license_no'] = 'Nein',
+  ['license_bought'] = 'Du kaufst die %s für ~r~%s€',
+  ['license_bought_fail'] = 'Du kannst dir kein %s leisten!',
+  ['license_weed'] = 'Gras Produzieren Lizenz',
+
+  -- blips
+  ['blip_weedfield'] = 'Gras Feld',
+  ['blip_weedprocessing'] = 'Gras Produzent',
+  ['blip_drugdealer'] = 'Drogen Dealer',
+}
