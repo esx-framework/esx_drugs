67886d6c9167ad15637186a1cf5963117f3d0ba1
C3h1c <fivemsloofficial@gmail.com>
2023-04-28 19:57:01 +0200

    Locale SL

---
+++ locales/sl.lua
@@ -0,0 +1,31 @@
+Locales ['sl'] = {
+  -- trava
+  ['weed_pickupprompt'] = 'Pritisnite [E] Da poberete Cannabis liste.',
+  ['weed_quitprocess'] = 'Pritisnite [E] Da ustavite Procesiranje Cannabis listov.',
+  ['weed_inventoryfull'] = 'Nimate dovolj prostora v vašem inventarju za Cannabis Liste.',
+  ['weed_processprompt'] = 'Pritisnite [E] Da začnete procesirati Cannabis liste.',
+  ['weed_processingstarted'] = 'Procesiranje Cannabisa v Marihuano...',
+  ['weed_processingfull'] = 'Proces ~r~zaustavljen ker ste polni tega',
+  ['weed_processingenough'] = 'Vi morate imeti 3 cannabis liste za ustvaritev.',
+  ['weed_processed'] = 'Vi ste naredili iz 3x Cannabisa 1x Marihuane',
+  ['weed_processingtoofar'] = 'Proces se je ~r~zaključil Ker ste odšli ven iz območja.',
+
+  -- drug dealer
+  ['dealer_prompt'] = 'Pritisnite [E] da se pogovorite s ~r~Drug Dealer-jem.',
+  ['dealer_title'] = 'Dealer Drog',
+  ['dealer_item'] = '$%s',
+  ['dealer_notenough'] = 'Vi nimate dovolj tega da bi prodali!',
+  ['dealer_sold'] = 'Vi ste prodali %sx %s za $%s',
+
+  -- licence
+  ['license_title'] = 'Vi potrebujete licenco za procesiranje trave če želite početi to dejavnost, kupite ga lahko tukaj.',
+  ['license_no'] = 'Ne',
+  ['license_bought'] = 'Vi ste kupili %s za ~r~$%s',
+  ['license_bought_fail'] = 'Vi si nemorate privoščiti %s!',
+  ['license_weed'] = 'Licenca za procesiranje trave',
+
+  -- blips
+  ['blip_weedfield'] = 'Del trave',
+  ['blip_weedprocessing'] = 'Procesiranje Trave',
+  ['blip_drugdealer'] = 'Dealer Drog',
+}
---
+++ localization/sl_esx_drugs.sql
@@ -0,0 +1,8 @@
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES
+	('cannabis', 'Cannabis', 3, 0, 1),
+	('marijuana', 'Marihuana', 2, 0, 1)
+;
+
+INSERT INTO `licenses` (`type`, `label`) VALUES
+	('weed_processing', 'Licenca za procesiranje trave')
+;
