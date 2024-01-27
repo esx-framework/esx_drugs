2e5d238ccd563e25ad68dc742b1590bb984add55
Dannezan <123192307+Dannezzzan@users.noreply.github.com>
2024-01-27 17:31:15 +0100

    Create sv_esx_drugs.sql
    Swedish esx_drugs SQL translation

---
+++ localization/sv_esx_drugs.sql
@@ -0,0 +1,8 @@
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES
+	('cannabis', 'Cannabis', 3, 0, 1),
+	('marijuana', 'Marijuana', 2, 0, 1)
+;
+
+INSERT INTO `licenses` (`type`, `label`) VALUES
+	('weed_processing', 'Weed processerings licens')
+;
